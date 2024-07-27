050223cf509586067349733b6e63c6be5dee9c3a
Steven <ssebexen@gmail.com>
2024-07-27 16:59:54 -0400

    Uses fallback data if API call fails.

--- frontend/scenes/title_screen/title_screen.gd
+++ frontend/scenes/title_screen/title_screen.gd
@@ -4,7 +4,7 @@ var story_screen = load("res://scenes/story_screen/story_screen.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Api.get_dialogue()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,9 +13,13 @@ func _process(delta):
 
 
 func _on_button_pressed():
-	var dialogue = Api.get_dialogue()
 	var story_screen_instance = story_screen.instantiate()
+	var dialogue = Api.data
+	if (not dialogue.has("romeo_dialogue") or not dialogue.has("juliet_dialogue")):
+		dialogue = Api.get_fallback_dialogue()
 	story_screen_instance.lines = [dialogue.romeo_dialogue, dialogue.juliet_dialogue]
+	if (not dialogue.has("color_hex_code")):
+		dialogue = Api.get_fallback_dialogue()
 	story_screen_instance.bullet_color = Color(dialogue.color_hex_code)
 	var story_screen_packed = PackedScene.new()
 	story_screen_packed.pack(story_screen_instance)
