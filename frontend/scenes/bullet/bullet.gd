16e98b1e9b7c1bce1def1c07a2d8434f360355a4
Steven <ssebexen@gmail.com>
2024-07-27 13:11:00 -0400

    Added bullets and one spawner: pinwheel

---
+++ frontend/scenes/bullet/bullet.gd
@@ -0,0 +1,23 @@
+extends RigidBody2D
+
+const BOUNDS_PADDING = 100
+var bounds
+@export var color: Color
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	bounds = [Vector2(-BOUNDS_PADDING, -BOUNDS_PADDING), get_viewport_rect().size + Vector2(BOUNDS_PADDING, BOUNDS_PADDING)]
+	$ColorRect.color = color
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	check_should_die()
+	
+func check_should_die():
+	var global_pos = get_global_transform().get_origin()
+	if (global_pos[0] < bounds[0][0]
+	or global_pos[0] > bounds[1][0]
+	or global_pos[1] < bounds[0][1]
+	or global_pos[1] > bounds[1][1]):
+		queue_free()
+
